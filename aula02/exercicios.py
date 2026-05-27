f701ff793ef64d585f7c2526ba8e633def4f7706
vitorspada <vitorspada.rs@gmail.com>
2026-05-26 23:21:44 -0300

    Realizando os outros exercícios

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -31,12 +31,34 @@ print(f'O quadrado de {quad} é: {quad ** 2}')
 
 # #### Números de Ponto Flutuante (`float`)
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+val_1 = float(input('Digite um número flutuante: '))
+val_2 = float(input('Digite outro número flutuante: '))
+soma_float = val_1 + val_2
+print(f'O resultado é {soma_float}')
 
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+med_1 = int(input('Digite um número: '))
+med_2 = int(input('Digite outro número: '))
+media = float(med_1 + med_2) / 2
+print(f'A média é: {media}')
 
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base_1 = int(input("Digite o número base: "))
+potencia_1 = int(input('Digite a potência: '))
+res_pot = float(base_1 ** potencia_1)
+print(f'O resultado da potência é: {res_pot}')
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+celsius = int(input('Digite a temperatura em Celsius: '))
+conversor = float((celsius * 9/5) + 32)
+print(f'A temperatura em Fahrenheit é: {conversor}')
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
+pi = math.pi
+raio = float(input('Digite o raio do cirulo'))
+area_circulo = pi * raio ** 2
+print(f'A área do circulo é: {area_circulo}')
 
 # #### Strings (`str`)
 
